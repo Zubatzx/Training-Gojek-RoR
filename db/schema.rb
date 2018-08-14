df199b8d6b41f7282a45da693049b97142eaf505
Zubatzx <adityaputrabudiman@yahoo.co.id>
2018-08-14 18:10:45 +0700

    Generate Scaffold untuk Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_065559) do
+ActiveRecord::Schema.define(version: 2018_08_14_110817) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
